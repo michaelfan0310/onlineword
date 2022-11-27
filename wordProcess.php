4204c8faf5b823fe0103ac6ca14c0e499d997127
marshall2023 <74441675+michaelfan0310@users.noreply.github.com>
2022-11-27 00:04:42 -0500

    first commit

---
+++ wordProcess.php
@@ -0,0 +1,55 @@
+<?php
+
+
+ 
+    $type=$_REQUEST['type'];
+    
+    $xmldoc=new DOMDocument();
+    $xmldoc->load("words.xml");
+    
+    
+    if($type=="query"){
+        //接受新的单词
+        $query_word=$_REQUEST['engword'];
+        
+        $words=$xmldoc->getElementsByTagName("word");
+        $isEnter=false;
+        for($i=0;$i<$words->length;$i++){
+            $word=$words->item($i);
+            $word_en=getNodeVal($word,"en");
+            
+            if($query_word==$word_en){                
+                $isEnter=true;
+                echo $query_word."&nbsp;对应中文是：".getNodeVal($word,"ch")."<br/>";
+            }
+        }       
+        if(!$isEnter){
+           echo "<br/>查询无此词条！！";
+    }
+    echo '<a href="wordsView.php">返回主界面</a>';
+    }elseif($type=="add"){
+        $root=$xmldoc->getElementsByTagName("words")->item(0);
+        $new_word=$xmldoc->createElement("word");
+        $new_word_en=$xmldoc->createElement("en");
+        $new_word_ch=$xmldoc->createElement("ch");
+        $new_word_en->nodeValue=$_REQUEST['engword'];
+        $new_word_ch->nodeValue=$_REQUEST['chword'];
+        $root->appendChild($new_word);
+        $new_word->appendChild($new_word_en);
+        $new_word->appendChild($new_word_ch);
+        
+        $b=$xmldoc->save("words.xml");
+        if(!$b){
+            echo "添加失败！！";
+        }else{
+            echo "添加成功！！";
+            }
+            echo "<br/>".'<a href="wordsView.php">返回主界面</a>';
+    }
+   
+    
+    function  getNodeVal(&$MyNode,$tagName){
+        return $MyNode->getElementsByTagName($tagName)->item(0)->nodeValue;
+    }
+     
+?>
---
+++ wordsView.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+ <title>金山词霸</title>
+  <style type="text/css">
+   
+  </style>
+
+</head>
+<img src="jinshan.png" width=15% />
+
+
+<h1>查询单词</h1>
+
+   <form action="wordProcess.php" method="post" >
+          请输入英文单词： <input type="text" name="engword" />
+              <input type="hidden" name="type"  value="query" />
+    		 <input type="submit" value="查询" />
+  
+    </form>
+ <h1>添加单词</h1>
+  <form action="wordProcess.php" method="post" >
+          请添加英文单词： <input type="text" name="engword" /><br/>
+           请添加中文注释： <input type="text" name="chword" /><br/>
+              <input type="hidden" name="type"  value="add" />
+    		 <input type="submit" value="添加" />
+  
+    </form>
+
+
+</html>
+
